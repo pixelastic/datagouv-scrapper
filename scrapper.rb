5d1d3793c36195180098f39f51aa56ce3b4ad5df
Pixelastic <tim@pixelastic.com>
2014-02-22 17:45:00 +0100

    Generating clean json.

--- scrapper.rb
+++ scrapper.rb
@@ -1,10 +1,10 @@
 # encoding : utf-8
-require "open-uri"
-# scrapper ministere-de-l-interieur
-# Télécharge organization_show
+require 'curb'
+require 'json'
 # Loop sur packages
 # télécharge le package
 # loop sur ressources
+# loop sur related_list
 
 
 class DataGouvScrapper
@@ -18,15 +18,87 @@ class DataGouvScrapper
     "#{@base_url}organization_show?id=#{organization}"
   end
 
-  def get_page_content_from_url(url)
-    open(url, 'r').read
+  def get_url_from_dataset(dataset)
+    "#{@base_url}package_show?id=#{dataset}"
   end
 
+  def get_page_content_from_url(url)
+    JSON.parse(Curl.get(url).body_str)
+  end
+
+  def get_tags_from_dataset(dataset)
+    return [] if (!dataset['result']['tags'])
+    tags = []
+    dataset['result']['tags'].each do |tag|
+      tags << tag['name']
+    end
+    return tags.uniq
+  end
+
+  def get_groups_from_dataset(dataset)
+    return [] if (!dataset['result']['groups'])
+    groups = []
+    dataset['result']['groups'].each do |group|
+      groups << group['name']
+    end
+    return groups.uniq
+  end
+
+  def get_resources_from_dataset(dataset)
+    return [] if (!dataset['result']['resources'])
+    resources = []
+    dataset['result']['resources'].each do |r|
+      resource = {
+        'created' => r['created'],
+        'revision_timestamp' => r['revision_timestamp'],
+        'name' => r['name'],
+        'description' => r['description'],
+        'format' => r['format'],
+        'url' => r['url']
+      }
+      resources << resource
+    end
+    return resources
+  end
 
   def run
-    content = get_page_content_from_url(get_url_from_organization(@organization));
-    puts content
+    data = []
+    organization = get_page_content_from_url(get_url_from_organization(@organization));
+    datasets = organization['result']['packages']
 
+    # i = 0
+    datasets.each do |d|
+      # i=i+1
+      # break if i >3
+
+      dataset = {
+        'name' => d['name'],
+        'notes' => d['notes'],
+        'title' => d['title']
+      }
+      
+      puts "Downloading #{d['title']}"
+      dataset_detail = get_page_content_from_url(get_url_from_dataset(d['name']))
+
+      dataset['territorial_coverage'] = dataset_detail['result']['territorial_coverage']
+      dataset['territorial_coverage_granularity'] = dataset_detail['result']['territorial_coverage_granularity']
+      dataset['temporal_coverage_from'] = dataset_detail['result']['temporal_coverage_from']
+      dataset['temporal_coverage_to'] = dataset_detail['result']['temporal_coverage_to']
+      dataset['license_title'] = dataset_detail['result']['license_title']
+      dataset['license_url'] = dataset_detail['result']['license_url']
+      dataset['revision_timestamp'] = dataset_detail['result']['revision_timestamp']
+
+      dataset['tags'] = get_tags_from_dataset(dataset_detail)
+      dataset['groups'] = get_groups_from_dataset(dataset_detail)
+      dataset['resources'] = get_resources_from_dataset(dataset_detail)
+
+      data << dataset
+    end
+
+    puts "Generating #{@organization}.json"
+		File.open("#{@organization}.json", "w") do |file|
+			file.write(JSON.pretty_generate(data))
+		end
   end
 
 
