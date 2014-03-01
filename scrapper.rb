b0afa05b8f57584d08b16fddda6a301f225f89c7
Pixelastic <tim@pixelastic.com>
2014-03-01 15:27:50 +0100

    Update scrapper to use new urls and catch more data.

--- scrapper.rb
+++ scrapper.rb
@@ -8,19 +8,16 @@ class DataGouvScrapper
   def initialize(*args)
     @organization = args[0];
     @base_url = "http://www.data.gouv.fr/api/3/action/"
+    @base_url_qa = "http://qa.data.gouv.fr/api/1/"
     @public_url = "http://www.data.gouv.fr/fr/"
   end
 
   def get_url_from_organization(organization)
-    "#{@base_url}organization_show?id=#{organization}"
+    "#{@base_url_qa}datasets?organization=#{organization}"
   end
 
-  def get_url_from_dataset_name(dataset_name)
-    "#{@base_url}package_show?id=#{dataset_name}"
-  end
-
-  def get_related_url_from_dataset_name(dataset_name)
-    "#{@base_url}related_list?id=#{dataset_name}"
+  def get_url_from_dataset_id(dataset_id)
+    "#{@base_url_qa}datasets/#{dataset_id}"
   end
 
   def get_public_url_from_dataset_name(dataset_name)
@@ -33,27 +30,27 @@ class DataGouvScrapper
   end
 
   def get_tags_from_dataset(dataset)
-    return [] if (!dataset['result']['tags'])
+    return [] if (!dataset['value']['tags'])
     tags = []
-    dataset['result']['tags'].each do |tag|
+    dataset['value']['tags'].each do |tag|
       tags << tag['name']
     end
     return tags.uniq
   end
 
   def get_groups_from_dataset(dataset)
-    return [] if (!dataset['result']['groups'])
+    return [] if (!dataset['value']['groups'])
     groups = []
-    dataset['result']['groups'].each do |group|
+    dataset['value']['groups'].each do |group|
       groups << group['name']
     end
     return groups.uniq
   end
 
   def get_resources_from_dataset(dataset)
-    return [] if (!dataset['result']['resources'])
+    return [] if (!dataset['value']['resources'])
     resources = []
-    dataset['result']['resources'].each do |r|
+    dataset['value']['resources'].each do |r|
       resource = {
         'created' => r['created'],
         'revision_timestamp' => r['revision_timestamp'],
@@ -68,38 +65,39 @@ class DataGouvScrapper
   end
 
   def get_related_from_dataset(dataset)
-    related_list = get_page_content_from_url(get_related_url_from_dataset_name(dataset['result']['name']))
-    return [] if (!related_list['result'])
+    return [] if (!dataset['value']['related'])
     final = []
-    related_list['result'].each do |r|
+    dataset['value']['related'].each do |r|
       related = {
         'description' => r['description'],
         'title' => r['title'],
         'url' => r['url'],
         'created' => r['created'],
         'type' => r['type'],
-        'image_url' => r['image_url']
+        'image_url' => r['image_url'],
+        'id' => r['id'],
       }
       final << related
     end
     return final
   end
 
-  def get_dataset_details_from_dataset_name(dataset_name)
+  def get_dataset_details_from_dataset_id(dataset_id)
       dataset = {}
-      dataset_detail = get_page_content_from_url(get_url_from_dataset_name(dataset_name))
+      dataset_detail = get_page_content_from_url(get_url_from_dataset_id(dataset_id))
 
-      dataset['name'] = dataset_detail['result']['name']
-      dataset['notes'] = dataset_detail['result']['notes']
-      dataset['title'] = dataset_detail['result']['title']
-      dataset['territorial_coverage'] = dataset_detail['result']['territorial_coverage']
-      dataset['territorial_coverage_granularity'] = dataset_detail['result']['territorial_coverage_granularity']
-      dataset['temporal_coverage_from'] = dataset_detail['result']['temporal_coverage_from']
-      dataset['temporal_coverage_to'] = dataset_detail['result']['temporal_coverage_to']
-      dataset['license_title'] = dataset_detail['result']['license_title']
-      dataset['license_url'] = dataset_detail['result']['license_url']
-      dataset['revision_timestamp'] = dataset_detail['result']['revision_timestamp']
-
+      dataset['name'] = dataset_detail['value']['name']
+      dataset['id'] = dataset_detail['value']['id']
+      dataset['notes'] = dataset_detail['value']['notes']
+      dataset['title'] = dataset_detail['value']['title']
+      dataset['territorial_coverage'] = dataset_detail['value']['territorial_coverage']
+      dataset['territorial_coverage_granularity'] = dataset_detail['value']['territorial_coverage_granularity']
+      dataset['temporal_coverage_from'] = dataset_detail['value']['temporal_coverage_from']
+      dataset['temporal_coverage_to'] = dataset_detail['value']['temporal_coverage_to']
+      dataset['license_title'] = dataset_detail['value']['license_title']
+      dataset['license_url'] = dataset_detail['value']['license_url']
+      dataset['revision_timestamp'] = dataset_detail['value']['revision_timestamp']
+      
       dataset['tags'] = get_tags_from_dataset(dataset_detail)
       dataset['groups'] = get_groups_from_dataset(dataset_detail)
       dataset['resources'] = get_resources_from_dataset(dataset_detail)
@@ -111,14 +109,14 @@ class DataGouvScrapper
   def get_organization_dataset(organization_name)
     data = []
     organization = get_page_content_from_url(get_url_from_organization(organization_name));
-    datasets = organization['result']['packages']
+    datasets = organization['value']
 
     i = 0
-    datasets.each do |d|
+    datasets.each do |id|
       i=i+1
       # break if i >3
-      puts "Downloading #{d['title']}"
-      data << get_dataset_details_from_dataset_name(d['name'])
+      puts "Downloading dataset #{id}"
+      data << get_dataset_details_from_dataset_id(id)
     end
 
 
@@ -175,7 +173,7 @@ class DataGouvScrapper
           dataset['title'],
           dataset['notes'],
           get_public_url_from_dataset_name(dataset['name']),
-          get_url_from_dataset_name(dataset['name']),
+          get_url_from_dataset_id(dataset['id']),
           get_files_for_csv_from_dataset(dataset),
           '',
           nil,
